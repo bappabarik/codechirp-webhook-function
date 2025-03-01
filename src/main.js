ec93fd0528f5ccc0a4d776b362a6a16650b6183b
Bappa Barik <61709328+bappabarik@users.noreply.github.com>
2025-03-01 19:43:13 +0530

    Refactor providerID assignment and enhance logging to include providerID type

--- src/main.js
+++ src/main.js
@@ -6,11 +6,11 @@ export default async ({ req, res, log, error }) => {
   if (req.path === "/webhook") {
     const event = req.headers["x-github-event"];
     if (event === 'push') {
-      const providerID = String(req.body.sender.id)
+      const providerID = req.body.sender.id
       const commits = req.body.commits
       for (const commit of commits) {
         const content = await getGroqChatCompletion(commit.message, "tweet")
-        log(commit.message, content);
+        log(commit.message, content, typeof providerID);
         if (!content) {
           error("error occurred during post creation")
         }
