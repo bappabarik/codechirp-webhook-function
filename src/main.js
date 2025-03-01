5d47c46aeb841e8a62b85159f71bf23864da0d50
Bappa Barik <61709328+bappabarik@users.noreply.github.com>
2025-03-01 17:25:38 +0530

    Refactor webhook handler to process push events and log request body

--- src/main.js
+++ src/main.js
@@ -5,23 +5,23 @@ export default async ({ req, res, log, error }) => {
 
   if (req.path === "/webhook") {
     const event = req.headers["x-github-event"];
-    const providerID = req.body.sender.id
-    // if (event === 'push') {
-    //   const commits = req.body.commits
-    //   for (const commit of commits) {
-    //     const content = await getGroqChatCompletion(commit.message, "tweet")
-    //     if (!content) {
-    //       error("error occurred during post creation")
-    //     }
-    //     const post = await databaseService.createPost({event, content, providerID, app: "X" })
-    //     if (post) {
-    //       console.log("post created successfully");
+    if (event === 'push') {
+      const providerID = req.body.sender.id
+      const commits = req.body.commits
+      for (const commit of commits) {
+        const content = await getGroqChatCompletion(commit.message, "tweet")
+        if (!content) {
+          error("error occurred during post creation")
+        }
+        const post = await databaseService.createPost({event, content, providerID, app: "X" })
+        if (post) {
+          console.log("post created successfully");
           
-    //     }
+        }
 
-    //   }
-    // }
-    log(event, providerID, req.body);
+      }
+    }
+    log(event, req.body);
     return res.send("WebHook Called")
   }
 
